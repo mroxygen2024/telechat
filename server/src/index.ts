66a6083b3a048f362519b2642309e91181c4a51c
mroxygen2024 <fuad.sano@astu.edu.et>
2026-02-08 22:12:38 +0300

    feat: error handled usng try/catch on server connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,15 @@ import { initSocket } from './sockets/index.js'
 const app = createApp()
 const server = http.createServer(app)
 
+server.on('error', (error: NodeJS.ErrnoException) => {
+  if (error.code === 'EADDRINUSE') {
+    console.error(`Port ${env.port} is already in use`)
+  } else {
+    console.error('Server error', error)
+  }
+  process.exit(1)
+})
+
 const start = async () => {
   await connectDb()
   initSocket(server)
